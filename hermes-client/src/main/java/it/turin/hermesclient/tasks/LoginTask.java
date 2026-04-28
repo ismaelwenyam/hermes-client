48bfc464532574ef42bee884d90612aa058e3b0c
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-28 22:04:14 +0200

    wip login task

--- hermes-client/src/main/java/it/turin/hermesclient/tasks/LoginTask.java
+++ hermes-client/src/main/java/it/turin/hermesclient/tasks/LoginTask.java
@@ -1,5 +1,7 @@
 package it.turin.hermesclient.tasks;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
 import it.turin.hermesclient.dto.Endpoint;
 import it.turin.hermesclient.dto.Request;
 import it.turin.hermesclient.dto.Response;
@@ -13,6 +15,7 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 public class LoginTask implements Callable<Response> {
@@ -28,23 +31,36 @@ public class LoginTask implements Callable<Response> {
 
     @Override
     public Response call() {
+        System.out.println("start login task");
         Socket socket = null;
-        ObjectOutputStream out = null;
-        ObjectInputStream in = null;
+        PrintStream out = null;
+        Scanner in = null;
+        Response response = null;
         try {
             socket = new Socket(inetAddress, port);
-            out = new ObjectOutputStream(socket.getOutputStream());
+            out = new PrintStream(socket.getOutputStream(), true);
             Map<String, Object> requestParams = new HashMap<>();
             requestParams.put("account", loginModel.getEmail());
             Request request = new Request(Endpoint.GET_USER, requestParams, null);
-            out.writeObject(request);
+            Gson gson = new Gson();
+            String jsonRequest = gson.toJson(request);
+            System.out.println("sending login request...");
+            out.println(jsonRequest);
+            System.out.println("sent login request");
             out.flush();
-            in = new ObjectInputStream(socket.getInputStream());
-            return (Response) in.readObject();
+            in = new Scanner(socket.getInputStream());
+            String line = "";
+            System.out.println("waiting server response...");
+            while (in.hasNextLine()) {
+                line = in.nextLine();
+            }
+            response = gson.fromJson(line, Response.class);
+            System.out.println("received response: " + response);
+            return response;
         } catch (IOException e) {
             System.err.println("exception in socket connection: " + e.getMessage());
-        } catch (ClassNotFoundException e) {
-            System.err.println("exception caught in casting response: " + e.getMessage());
+        } catch (JsonIOException e) {
+            System.err.println("json exception: " + e.getMessage());
         } finally {
             try {
                 if (out != null) out.close();
