d42e1eb744e8372d3bcf85bde9df6df43b5916cc
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-28 20:45:39 +0200

    start implementation user login

--- hermes-client/src/main/java/it/turin/hermesclient/controller/LoginController.java
+++ hermes-client/src/main/java/it/turin/hermesclient/controller/LoginController.java
@@ -1,16 +1,25 @@
 package it.turin.hermesclient.controller;
 
+import it.turin.hermesclient.dto.Response;
 import it.turin.hermesclient.model.LoginModel;
+import it.turin.hermesclient.tasks.LoginTask;
 import it.turin.hermesclient.utils.EmailValidator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 public class LoginController {
     private LoginModel loginModel;
 
+    @FXML
+    public ProgressBar progessBar;
     @FXML
     public TextField emailField;
     @FXML
@@ -24,11 +33,27 @@ public class LoginController {
     }
 
     public void login(MouseEvent mouseEvent) {
-        if (!EmailValidator.isValid(emailField.getText().trim())){
+        if (!EmailValidator.isValid(emailField.getText().trim()))
             showErrorLabel("Email not valid");
-            return;
+        else
+            logUser();
+    }
+
+    private void logUser() {
+        try {
+            FutureTask<Response> futureResponse = new FutureTask<Response>(new LoginTask(InetAddress.getLocalHost(), 8080, loginModel));
+            Thread t = new Thread(futureResponse);
+            t.start();
+            progessBar.setVisible(true);
+            Response response = futureResponse.get();
+            System.out.println("response: " + response);
+        } catch (UnknownHostException e) {
+            System.err.println("exception in get local host: " + e.getMessage());
+        } catch (ExecutionException | InterruptedException e) {
+            System.err.println("exeception caught in execution tuture task: " + e.getMessage());
         }
     }
+
 
     private void showErrorLabel(String error) {
         errorLabel.setText(error);
