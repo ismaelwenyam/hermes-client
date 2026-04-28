c3ea46734ff719207828c5b45eb4bb366029a85c
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-28 22:03:42 +0200

    opened client dto to gson

--- hermes-client/src/main/java/module-info.java
+++ hermes-client/src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module it.turin.hermesclient {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.google.gson;
 
     opens it.turin.hermesclient to javafx.fxml;
     opens it.turin.hermesclient.controller to javafx.fxml;
+    opens it.turin.hermesclient.dto to com.google.gson;
     exports it.turin.hermesclient;
 }
