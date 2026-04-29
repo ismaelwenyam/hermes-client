836cd47f5d28e9a9f7a01e6d7cdcb3803bbc8a6c
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-29 23:25:57 +0200

    exported model to gson

--- hermes-client/src/main/java/module-info.java
+++ hermes-client/src/main/java/module-info.java
@@ -6,5 +6,6 @@ module it.turin.hermesclient {
     opens it.turin.hermesclient to javafx.fxml;
     opens it.turin.hermesclient.controller to javafx.fxml;
     opens it.turin.hermesclient.dto to com.google.gson;
+    opens it.turin.hermesclient.model to com.google.gson;
     exports it.turin.hermesclient;
 }
