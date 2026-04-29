a10ab3d2098d422658b418ef87055c995f3ae0cf
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-29 21:38:11 +0200

    implemented scheduled task executor

--- hermes-client/src/main/java/it/turin/hermesclient/tasks/ScheduledTasksExecutor.java
+++ hermes-client/src/main/java/it/turin/hermesclient/tasks/ScheduledTasksExecutor.java
@@ -1,4 +1,28 @@
 package it.turin.hermesclient.tasks;
 
+import it.turin.hermesclient.model.ClientModel;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class ScheduledTasksExecutor {
+    private final ClientModel clientModel;
+    private final int port;
+    private final ScheduledExecutorService scheduledExecs;
+
+    public ScheduledTasksExecutor(ClientModel clientModel, int port) {
+        this.clientModel = clientModel;
+        this.port = port;
+        this.scheduledExecs = Executors.newScheduledThreadPool(2);
+    }
+
+    public void start () {
+        Connection connection = new Connection(clientModel, port);
+        scheduledExecs.scheduleAtFixedRate(connection, 0, 10, TimeUnit.SECONDS);
+    }
+
+    public void stop () {
+        scheduledExecs.shutdown();
+    }
 }
