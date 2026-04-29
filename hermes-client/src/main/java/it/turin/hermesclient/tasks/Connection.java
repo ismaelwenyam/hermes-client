4f7b8e249a081ad3f0e12169ace9db22b16adf78
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-29 21:37:40 +0200

    implemented connection task

--- hermes-client/src/main/java/it/turin/hermesclient/tasks/Connection.java
+++ hermes-client/src/main/java/it/turin/hermesclient/tasks/Connection.java
@@ -1,12 +1,42 @@
 package it.turin.hermesclient.tasks;
 
 import it.turin.hermesclient.model.ClientModel;
+import javafx.application.Platform;
+import javafx.scene.paint.Color;
 
-public class CheckConnection implements Runnable {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class Connection implements Runnable {
     private final ClientModel clientModel;
+    private final int port;
+
+    public Connection (ClientModel clientModel, int port) {
+        this.clientModel = clientModel;
+        this.port = port;
+    }
 
     @Override
     public void run() {
-
+        try (Socket socket = new Socket()) {
+            System.out.println("checking server status");
+            socket.connect(new InetSocketAddress(InetAddress.getLocalHost(), port), 3000);
+            Platform.runLater(() -> {
+                clientModel.setServerOn(Color.GREEN);
+            });
+        } catch (SocketTimeoutException e) {
+            System.err.println("socket connection timeout: " + e.getMessage());
+            Platform.runLater(() -> {
+                clientModel.setServerOn(Color.RED);
+            });
+        } catch (IOException e) {
+            System.err.println("io exception in socket connection: " + e.getMessage());
+            Platform.runLater(() -> {
+                clientModel.setServerOn(Color.RED);
+            });
+        }
     }
 }
