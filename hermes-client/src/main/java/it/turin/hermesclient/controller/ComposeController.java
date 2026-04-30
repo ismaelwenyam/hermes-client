4ef4a2e11a6c11957210decb8362e6c6d74a32a5
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-30 16:20:35 +0200

    wip on send action

--- hermes-client/src/main/java/it/turin/hermesclient/controller/ComposeController.java
+++ hermes-client/src/main/java/it/turin/hermesclient/controller/ComposeController.java
@@ -1,20 +1,49 @@
 package it.turin.hermesclient.controller;
 
+import it.turin.hermesclient.model.ClientModel;
+import it.turin.hermesclient.utils.EmailValidator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ComposeController {
+    private ClientModel clientModel;
+
     @FXML public TextField argumentTxtField;
     @FXML public TextField recipientsTxtField;
     @FXML public TextArea mailTxtArea;
     @FXML public Label errorLabel;
 
+    public void init (ClientModel clientModel) {
+        this.clientModel = clientModel;
+    }
+
     public void onCancel(MouseEvent mouseEvent) {
+        //TODO turn to HomeView
     }
 
     public void onSend(MouseEvent mouseEvent) {
+        if (recipientsTxtField.getText().trim().isEmpty()){
+            errorLabel.setVisible(true);
+            errorLabel.setText("Nessun destinatario inserito");
+            return;
+        }
+        List<String> recipients = Arrays.stream(recipientsTxtField.getText().split(";")).toList();
+        System.out.println("recipients: " + recipients);
+        if (!EmailValidator.allValid(recipients)) {
+            errorLabel.setVisible(true);
+            errorLabel.setText(EmailValidator.findInvalid(recipients).toString());
+            return;
+        }
+        if (argumentTxtField.getText().trim().isEmpty()) {
+            errorLabel.setVisible(true);
+            errorLabel.setText("Nessun oggetto");
+        }
+
     }
 }
