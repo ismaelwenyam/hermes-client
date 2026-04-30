a83d1501aedf9c24e7914db5df25cf2fb762a0c1
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-30 17:29:25 +0200

    wip compose controller

--- hermes-client/src/main/java/it/turin/hermesclient/controller/ComposeController.java
+++ hermes-client/src/main/java/it/turin/hermesclient/controller/ComposeController.java
@@ -1,6 +1,9 @@
 package it.turin.hermesclient.controller;
 
 import it.turin.hermesclient.model.ClientModel;
+import it.turin.hermesclient.model.ComposeModel;
+import it.turin.hermesclient.model.Email;
+import it.turin.hermesclient.tasks.Forwarding;
 import it.turin.hermesclient.utils.EmailValidator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -8,11 +11,14 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class ComposeController {
     private ClientModel clientModel;
+    private ComposeModel composeModel;
 
     @FXML public TextField argumentTxtField;
     @FXML public TextField recipientsTxtField;
@@ -21,6 +27,7 @@ public class ComposeController {
 
     public void init (ClientModel clientModel) {
         this.clientModel = clientModel;
+        composeModel = new ComposeModel();
     }
 
     public void onCancel(MouseEvent mouseEvent) {
@@ -44,6 +51,17 @@ public class ComposeController {
             errorLabel.setVisible(true);
             errorLabel.setText("Nessun oggetto");
         }
+        Email mail = new Email("ismael@hermes.it", recipients, argumentTxtField.getText().trim(), mailTxtArea.getText().trim(), Date.from(Instant.now()));
+        composeModel.setMail(mail);
+        send();
+        argumentTxtField.clear();
+        recipientsTxtField.clear();
+        mailTxtArea.clear();
+        errorLabel.setVisible(false);
+    }
 
+    private void send () {
+        Thread sendThread = new Thread(new Forwarding(composeModel, 8080), "forwarding");
+        sendThread.start();
     }
 }
