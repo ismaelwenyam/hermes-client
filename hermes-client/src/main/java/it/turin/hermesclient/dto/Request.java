71f8430c756f61ffedc6496974454c31645d2429
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-30 17:28:53 +0200

    made request generic class

--- hermes-client/src/main/java/it/turin/hermesclient/dto/Request.java
+++ hermes-client/src/main/java/it/turin/hermesclient/dto/Request.java
@@ -1,37 +1,16 @@
 package it.turin.hermesclient.dto;
 
-import java.io.Serial;
-import java.io.Serializable;
 import java.util.Map;
 
-public class Request implements Serializable {
-    @Serial
-    private static final long serialVersionUID = -21548922354897L;
+public class Request<T> {
 
-    private final Endpoint endpoint;
-    private final Map<String, Object> requestParameters;
-    private final Object body;
+    private Endpoint endpoint;
+    private Map<String, Object> requestParameters;
+    private T body;
 
-    public Request (Endpoint endpoint, Map<String, Object> requestParameters, Object body) {
+    public Request (Endpoint endpoint, Map<String, Object> requestParameters, T body) {
         this.endpoint = endpoint;
         this.requestParameters = requestParameters;
         this.body = body;
-    }
-
-    public Endpoint getEndpoint() {return this.endpoint;}
-    public String getRequestParameter(String par) {
-        return (String) requestParameters.get(par);
-    }
-    public Object getBody() {return this.body;}
-
-    public boolean validParams (String... params) {
-        for (String param:
-                params) {
-            if (param == null || param.isBlank()) return false;
-        }
-        return true;
-    }
-    public boolean validObject() {
-        throw new UnsupportedOperationException("TODO");
     }
 }
