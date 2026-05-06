ee34c3a283bf9809d48ff3f155470dbada2c0bea
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-05-06 17:45:25 +0200

    refactored login process

--- hermes-client/src/main/java/it/turin/hermesclient/controller/LoginController.java
+++ hermes-client/src/main/java/it/turin/hermesclient/controller/LoginController.java
@@ -3,6 +3,7 @@ package it.turin.hermesclient.controller;
 import it.turin.hermesclient.model.ClientModel;
 import it.turin.hermesclient.tasks.Login;
 import it.turin.hermesclient.utils.EmailValidator;
+import it.turin.hermesclient.utils.SceneManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,8 +12,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
-public class LoginController {
+import java.io.IOException;
+
+public class LoginController extends ClientController {
     private ClientModel clientModel;
+    private Login login;
 
     @FXML
     public VBox loginView;
@@ -25,21 +29,16 @@ public class LoginController {
     @FXML
     public Button loginButton;
 
-    public boolean emailValid = false;
-
     public void init (ClientModel clientModel) {
         this.clientModel = clientModel;
         clientModel.emailProperty().bind(emailField.textProperty());
-        loginView.visibleProperty().bind(clientModel.userNotLoggedInProperty());
-        errorLabel.visibleProperty().bind(clientModel.showErrorProperty());
-        errorLabel.textProperty().bind(clientModel.errorMessageProperty());
     }
 
     public void onLoginButton(MouseEvent mouseEvent) {
         loginButton.setVisible(false);
         if (!EmailValidator.isValid(emailField.getText().trim())) {
-            clientModel.setErrorMessage("Email not valid");
-            clientModel.setShowError(true);
+            errorLabel.setText("Email not valid");
+            errorLabel.setVisible(true);
             loginButton.setVisible(true);
         }
         else
@@ -47,27 +46,30 @@ public class LoginController {
     }
 
     private void logUser() {
-        Thread t = new Thread(new Login(clientModel, 8080), "login-thread");
+        login = new Login(clientModel, 8080);
+        login.setOnSucceeded(e -> {
+            if (login.getValue()) {
+                try {
+                    SceneManager.switchScene("home-view.fxml", clientModel);
+                } catch (IOException ex) {
+                    System.err.println("exception switching scene");
+                }
+            } else {
+                errorLabel.setText(login.getMessage());
+                errorLabel.setVisible(true);
+            }
+        });
+        login.setOnFailed(e -> {
+            errorLabel.setText("client error");
+            errorLabel.setVisible(true);
+        });
+
+        Thread t = new Thread(login, "login-thread");
         t.start();
     }
 
     public void hideErrorLabel(MouseEvent mouseEvent) {
         loginButton.setVisible(true);
-        clientModel.setShowError(false);
+        errorLabel.setVisible(false);
     }
-
-    /*
-    public void onEnterPressed(KeyEvent keyEvent) {
-        if (keyEvent.getCode().equals(KeyCode.ENTER)){
-            loginButton.setVisible(false);
-            if (!EmailValidator.isValid(emailField.getText().trim())) {
-                clientModel.setErrorMessage("Email not valid");
-                clientModel.setShowError(true);
-                loginButton.setVisible(true);
-            }
-            else
-                logUser();
-        }
-    }
-    */
 }
--- hermes-client/src/main/java/it/turin/hermesclient/tasks/Login.java
+++ hermes-client/src/main/java/it/turin/hermesclient/tasks/Login.java
@@ -1,25 +1,19 @@
 package it.turin.hermesclient.tasks;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
 import it.turin.hermesclient.dto.Endpoint;
 import it.turin.hermesclient.dto.Request;
 import it.turin.hermesclient.dto.Response;
 import it.turin.hermesclient.model.ClientModel;
 import it.turin.hermesclient.network.ServerConnection;
-import it.turin.hermesclient.utils.SceneManager;
-import javafx.application.Platform;
-import javafx.scene.paint.Color;
+import javafx.concurrent.Task;
 
 import java.io.*;
 import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
-public class Login implements Runnable {
+public class Login extends Task<Boolean> {
     private final ClientModel clientModel;
     private final int port;
 
@@ -29,7 +23,7 @@ public class Login implements Runnable {
     }
 
     @Override
-    public void run () {
+    public Boolean call () {
         System.out.println("start login task");
         Map<String, Object> requestParams = new HashMap<>();
         requestParams.put("account", clientModel.getEmail());
@@ -38,48 +32,23 @@ public class Login implements Runnable {
         String jsonRequest = gson.toJson(request);
         String jsonResponse;
         try {
-            jsonResponse = ServerConnection.sendRequest(jsonRequest, InetAddress.getLocalHost().toString(), port);
-        } catch (UnknownHostException e) {
-            System.err.println("unkown host exception in loging in: " + e.getMessage());
-            Platform.runLater(() -> {
-                clientModel.setShowError(true);
-                clientModel.setErrorMessage("internal server error");
-            });
-            return;
+            jsonResponse = ServerConnection.sendRequest(jsonRequest, InetAddress.getLocalHost().getHostAddress(), port);
         } catch (IOException e) {
             System.err.println("server unavailable in loging in: " + e.getMessage());
-            Platform.runLater(() -> {
-                clientModel.setShowError(true);
-                clientModel.setErrorMessage("internal server error");
-            });
-            return;
+            updateMessage("connection refused");
+            return false;
         }
         Response response = gson.fromJson(jsonResponse, Response.class);
         if (response == null) {
-            Platform.runLater(() -> {
-                clientModel.setShowError(true);
-                clientModel.setErrorMessage("internal server error");
-            });
-            return;
+            updateMessage("internal server error");
+            return false;
         }
         System.out.println("received response: " + response);
         if (response.getStatusCode() == 200) {
-            Platform.runLater(() -> {
-                //clientModel.setUserNotLoggedIn(false);
-                //clientModel.setUserLoggedIn(true);
-                try {
-                    SceneManager.switchScene("home-view.fxml", clientModel);
-                } catch (IOException e) {
-                    System.err.println("execption in switching scene");
-                }
-            });
-        } else {
-            Platform.runLater(() -> {
-                clientModel.setShowError(true);
-                clientModel.setErrorMessage("invalid mail");
-            });
+            return true;
         }
-
+        updateMessage("email don't exists");
         System.out.println("end login task");
+        return false;
     }
 }
