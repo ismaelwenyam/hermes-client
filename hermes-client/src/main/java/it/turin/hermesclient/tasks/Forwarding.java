d7f8191e317e29f83bc7fe7c793ce92232eeabf1
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-05-08 14:03:53 +0200

    refactored

--- hermes-client/src/main/java/it/turin/hermesclient/tasks/Forwarding.java
+++ hermes-client/src/main/java/it/turin/hermesclient/tasks/Forwarding.java
@@ -1,37 +1,28 @@
 package it.turin.hermesclient.tasks;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
 import com.google.gson.reflect.TypeToken;
 import it.turin.hermesclient.dto.Endpoint;
 import it.turin.hermesclient.dto.Request;
 import it.turin.hermesclient.dto.Response;
 import it.turin.hermesclient.model.ClientModel;
-import it.turin.hermesclient.model.ComposeModel;
 import it.turin.hermesclient.model.Email;
 import it.turin.hermesclient.network.ServerConnection;
 import javafx.application.Platform;
 import javafx.scene.paint.Color;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.lang.reflect.Type;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.List;
-import java.util.Scanner;
 
 public class Forwarding implements Runnable {
     private final ClientModel clientModel;
-    private final ComposeModel composeModel;
     private final int port;
 
-    public Forwarding(ClientModel clientModel, ComposeModel composeModel, int port) {
+    public Forwarding(ClientModel clientModel, int port) {
         this.clientModel = clientModel;
-        this.composeModel = composeModel;
         this.port = port;
     }
 
@@ -40,20 +31,22 @@ public class Forwarding implements Runnable {
     public void run() {
         //TODO if server is offline do something
         System.out.println("start forwarding task");
-        Request<Email> request = new Request<>(Endpoint.POST_EMAIL, null, composeModel.getMail());
+        Request<Email> request = new Request<>(Endpoint.POST_EMAIL, null, clientModel.getMail());
         Gson gson = new Gson();
         String jsonRequest = gson.toJson(request);
         String jsonResponse;
         try {
-            jsonResponse = ServerConnection.sendRequest(jsonRequest, InetAddress.getLocalHost().toString(), port);
+            jsonResponse = ServerConnection.sendRequest(jsonRequest, InetAddress.getLocalHost().getHostAddress(), port);
         } catch (UnknownHostException e) {
-            System.err.println("unkown host exception in pooling: " + e.getMessage());
+            System.err.println("unkown host exception in forwarding: " + e.getMessage());
             return;
         } catch (IOException e) {
-            System.err.println("server unavailable in pooling: " + e.getMessage());
+            System.err.println("server unavailable in forwarding: " + e.getMessage());
             Platform.runLater(() -> {
-                homeModel.updateServerStatus(false);
-                clientModel.setServerOn(Color.RED);
+                clientModel.updateServerStatus(false);
+                clientModel.setServerStatusColor(Color.RED);
+                clientModel.setErrorMessage("service unavailable");
+                clientModel.setShowError(true);
             });
             return;
         }
@@ -68,7 +61,8 @@ public class Forwarding implements Runnable {
             Platform.runLater(() -> {
                 clientModel.setArgument("");
                 clientModel.setRecipients("");
-                composeModel.setTextBody("");
+                clientModel.setTextBody("");
+                clientModel.setShowError(false);
             });
         } else {
             Type emailListType = new TypeToken<List<String>>() {}.getType();
@@ -80,13 +74,13 @@ public class Forwarding implements Runnable {
             Platform.runLater(() -> {
                 clientModel.setErrorMessage("not found: " + emails);
                 clientModel.setShowError(true);
-                clientModel.setArgument(composeModel.getMail().getArgument());
+                clientModel.setArgument(clientModel.getMail().getArgument());
                 String unvalidEmails = "";
                 for (String e : emails){
                     unvalidEmails = unvalidEmails.concat(e + ";");
                 }
-                clientModel.setRecipients(unvalidEmails);
-                composeModel.setTextBody(composeModel.getMail().getMailBody());
+                //clientModel.setRecipients(unvalidEmails);
+                clientModel.setTextBody(clientModel.getMail().getMailBody());
             });
             System.out.println("something went wrong in response from server");
         }
