9c88457fc8d37054a48514293e5728506fd2044e
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-05-08 14:06:02 +0200

    refactored

--- hermes-client/src/main/java/it/turin/hermesclient/controller/LoginController.java
+++ hermes-client/src/main/java/it/turin/hermesclient/controller/LoginController.java
@@ -7,7 +7,6 @@ import it.turin.hermesclient.utils.SceneManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import java.io.IOException;
