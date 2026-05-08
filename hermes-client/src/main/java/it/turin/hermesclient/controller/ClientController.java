03cfc2e7e3bcdbf351ce343558e61d42b6c0f678
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-05-08 14:06:58 +0200

    impl abstract controller

---
+++ hermes-client/src/main/java/it/turin/hermesclient/controller/ClientController.java
@@ -0,0 +1,8 @@
+package it.turin.hermesclient.controller;
+
+import it.turin.hermesclient.model.ClientModel;
+
+public abstract class ClientController {
+
+    public abstract void init(ClientModel clientModel);
+}
