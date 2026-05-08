38f304a9e4d37c5648ef1691f8e492bbe4526c38
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-05-08 14:03:06 +0200

    refactored

--- hermes-client/src/main/java/it/turin/hermesclient/tasks/Ping.java
+++ hermes-client/src/main/java/it/turin/hermesclient/tasks/Ping.java
@@ -1,7 +1,6 @@
 package it.turin.hermesclient.tasks;
 
 import it.turin.hermesclient.model.ClientModel;
-import it.turin.hermesclient.model.HomeModel;
 import it.turin.hermesclient.network.ServerConnection;
 import javafx.application.Platform;
 import javafx.scene.paint.Color;
@@ -11,28 +10,26 @@ import java.net.InetAddress;
 
 public class Ping implements Runnable {
     private final ClientModel clientModel;
-    private final HomeModel homeModel;
     private final int port;
 
-    public Ping(ClientModel clientModel, HomeModel homeModel, int port) {
+    public Ping(ClientModel clientModel, int port) {
         this.clientModel = clientModel;
-        this.homeModel = homeModel;
         this.port = port;
     }
 
     @Override
     public void run() {
         try {
-            ServerConnection.ping(InetAddress.getLocalHost().toString(), port, 3000);
+            ServerConnection.ping(InetAddress.getLocalHost().getHostAddress(), port, 3000);
             Platform.runLater(() -> {
-                homeModel.updateServerStatus(true);
-                clientModel.setServerOn(Color.GREEN);
+                clientModel.updateServerStatus(true);
+                clientModel.setServerStatusColor(Color.GREEN);
             });
         } catch (IOException e) {
             System.err.println("socket connection timeout: " + e.getMessage());
             Platform.runLater(() -> {
-                homeModel.updateServerStatus(false);
-                clientModel.setServerOn(Color.RED);
+                clientModel.updateServerStatus(false);
+                clientModel.setServerStatusColor(Color.RED);
             });
         }
     }
