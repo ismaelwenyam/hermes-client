f882b9535d64f4da84a367b8bc4010c188ce2d8e
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-05-26 11:31:55 +0200

    made count a background activity depending on server availability

--- hermes-client/src/main/java/it/turin/hermesclient/tasks/Count.java
+++ hermes-client/src/main/java/it/turin/hermesclient/tasks/Count.java
@@ -14,7 +14,6 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Count implements Runnable {
     private static final Gson gson = new Gson();
@@ -29,43 +28,50 @@ public class Count implements Runnable {
     @Override
     public void run() {
         System.out.println("start count");
-        if (!clientModel.isServerLive()) return;
-        Map<String, Object> requestParams = new HashMap<>();
-        requestParams.put("account", clientModel.getEmail());
-        Request<Void> request  =new Request<>(Endpoint.COUNT, requestParams, null);
-        String jsonRequest = gson.toJson(request);
-        String jsonResponse;
-        try {
-            jsonResponse = ServerConnection.sendRequest(jsonRequest, InetAddress.getLocalHost().getHostAddress(), port);
-        } catch (UnknownHostException e) {
-            System.err.println("unkown host exception in count: " + e.getMessage());
-            return;
-        } catch (IOException e) {
-            System.err.println("server unavailable in count: " + e.getMessage());
-            Platform.runLater(() -> {
-                clientModel.updateServerStatus(false);
-                clientModel.setServerStatusColor(Color.RED);
-                clientModel.setErrorMessage("service unavailable");
-                clientModel.setShowError(true);
-            });
-            return;
-        }
-        System.out.println("received count response: " + jsonResponse);
-        Response<?> response = gson.fromJson(jsonResponse, Response.class);
-        if (response == null) {
-            System.out.println("something went wrong in response from server");
-            return;
-        }
-        System.out.println("received count response: " + response);
-        if (response.getStatusCode() == 200) {
-            Number n = (Number) response.getResponseBody();
-            long newCount = n.longValue();
-            if (newCount > Long.parseLong(clientModel.getEmailsCount())){
-                clientModel.getPool().release();
+        while (true) {
+            try {
+                clientModel.getCountingSem().acquire();
+            } catch (InterruptedException e) {
+                System.err.println("exeception occured in counting: " + e.getMessage() + " " + e);
+                return;
             }
-        } else {
-            System.out.println("server error, status: " + response.getStatusCode());
+            Map<String, Object> requestParams = new HashMap<>();
+            requestParams.put("account", clientModel.getEmail());
+            Request<Void> request  =new Request<>(Endpoint.COUNT, requestParams, null);
+            String jsonRequest = gson.toJson(request);
+            String jsonResponse;
+            try {
+                jsonResponse = ServerConnection.sendRequest(jsonRequest, InetAddress.getLocalHost().getHostAddress(), port);
+            } catch (UnknownHostException e) {
+                System.err.println("unkown host exception in count: " + e.getMessage());
+                return;
+            } catch (IOException e) {
+                System.err.println("server unavailable in count: " + e.getMessage());
+                Platform.runLater(() -> {
+                    clientModel.updateServerStatus(false);
+                    clientModel.setServerStatusColor(Color.RED);
+                    clientModel.setErrorMessage("service unavailable");
+                    clientModel.setShowError(true);
+                });
+                return;
+            }
+            System.out.println("received count response: " + jsonResponse);
+            Response<?> response = gson.fromJson(jsonResponse, Response.class);
+            if (response == null) {
+                System.out.println("something went wrong in response from server");
+                return;
+            }
+            System.out.println("received count response: " + response);
+            if (response.getStatusCode() == 200) {
+                Number n = (Number) response.getResponseBody();
+                long newCount = n.longValue();
+                if (newCount > Long.parseLong(clientModel.getEmailsCount())){
+                    clientModel.getPoolingSem().release();
+                }
+            } else {
+                System.out.println("server error, status: " + response.getStatusCode());
+            }
+            System.out.println("end count task");
         }
-        System.out.println("end count task");
     }
 }
